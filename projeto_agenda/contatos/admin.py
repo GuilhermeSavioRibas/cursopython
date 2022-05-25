24928d337d09c0f1e8cc21ed99cf559935383135
GuilhermeSavioRibas <guilherme.savio@gmail.com>
2022-05-25 14:18:03 -0400

    Exibindo os valores na home e adicionado paginação.

--- projeto_agenda/contatos/admin.py
+++ projeto_agenda/contatos/admin.py
@@ -4,11 +4,12 @@ from .models import Categoria, Contato
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
-    list_filter = ('nome', 'sobrenome')
+    list_filter = ('categoria',)
     list_per_page = 10
-    search_fields = ('nome', 'sobrenome', 'telefone')
+    search_fields = ('nome', 'sobrenome', 'telefone', 'email')
+    list_editable = ('telefone', 'mostrar')
 
 
 admin.site.register(Categoria)
